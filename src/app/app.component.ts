d62054811335978c051a9d8175bce89c4e7280f4
Kevin <kevin.wissmer@mail.de>
2022-05-22 13:49:38 +0200

    exapmle sidenav deleted css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  navbarIcon = 'navigate_next';
+
   title = 'pokedexGruppenarbeit';
 
-  changeIcon(){
-    if(this.navbarIcon == 'chevron_left'){
-      this.navbarIcon = 'navigate_next';
-    }else{
-      this.navbarIcon = 'chevron_left'
-    }
-  }
 }
