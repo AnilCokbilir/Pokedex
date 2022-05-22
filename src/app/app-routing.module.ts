55b2e9b04479b1db6b54c00c36e649af855a6328
Kevin <kevin.wissmer@mail.de>
2022-05-22 14:50:21 +0200

    startscreen structur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokedexComponent } from './pokedex/pokedex.component';
+import { StartscreenComponent } from './startscreen/startscreen.component';
 
 const routes: Routes = [
-  { path: 'pokedex', component: PokedexComponent }
+  { path: 'pokedex', component: PokedexComponent },
+  { path: '', component: StartscreenComponent }
 ];
 
 @NgModule({
