2f4600ab31f9087de1fcec65e14b795d28f8544d
Kevin <kevin.wissmer@mail.de>
2022-05-24 05:30:59 +0200

    type icons added

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -4,7 +4,9 @@ import { FetchApiService } from '../fetch-api.service';
 @Component({
   selector: 'app-startscreen',
   templateUrl: './startscreen.component.html',
-  styleUrls: ['./startscreen.component.scss']
+  styleUrls: ['./startscreen.component.scss',
+  '../app.component.scss'
+]
 })
 export class StartscreenComponent implements OnInit {
 
@@ -14,6 +16,12 @@ export class StartscreenComponent implements OnInit {
     this.showConfig()
   }
 
+  pkmOfDay = {
+    name: "bulbasaur",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+    id: 1,
+    types: [{type: {name: "grass"}},{type: {name: "poison"}}]
+  }
   showConfig() {
     this.api.getConfig()
       .subscribe((api: any) => {
