629f21756555f54336dd4606878f96c441bd59c9
Kevin <kevin.wissmer@mail.de>
2022-05-25 12:21:55 +0200

    routing for pokedexDetailView added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokemonDetailViewComponent } from './pokemon-detail-view/pokemon-detail-view.component';
 import { StartscreenComponent } from './startscreen/startscreen.component';
 
 const routes: Routes = [
   { path: 'pokedex', component: PokedexComponent },
+  { path: 'pokedexDetail', component: PokemonDetailViewComponent },
   { path: '', component: StartscreenComponent }
 ];
 
