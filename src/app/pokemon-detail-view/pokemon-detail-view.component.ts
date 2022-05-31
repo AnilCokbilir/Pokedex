1d395ce2917e2bc1a204150874e268a39689512c
Kevin <kevin.wissmer@mail.de>
2022-05-31 20:56:38 +0200

    Poketypes addon detail view

--- src/app/pokemon-detail-view/pokemon-detail-view.component.ts
+++ src/app/pokemon-detail-view/pokemon-detail-view.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pokemon-detail-view',
   templateUrl: './pokemon-detail-view.component.html',
-  styleUrls: ['./pokemon-detail-view.component.scss']
+  styleUrls: ['./pokemon-detail-view.component.scss',
+  '../app.component.scss']
 })
 export class PokemonDetailViewComponent implements OnInit {
-
+ img_pkm = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg";
   constructor() { }
-
+  types = ["bug","grass"]
   ngOnInit(): void {
   }
 
